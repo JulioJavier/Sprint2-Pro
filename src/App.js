978952486b9abbed55c77efc2f22a85399ce6918
JulioJavier <01juliomtz@gmail.com>
2023-01-25 01:29:47 -0500

    actualizacion del repo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
-
 function App() {
   return (
-    <div className="App">
-      <p>ya estoy listo</p>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
