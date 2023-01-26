e54a1740af557d1f5668040fb1045905751f29a9
JulioJavier <01juliomtz@gmail.com>
2023-01-26 00:39:16 -0500

    feact: actualizando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <h1 className="text-6xl text-yellow-700">JaJaJaJAjA!</h1>
+      <h1 className="text-6xl text-yellow-700">Lo Sentimos</h1>
+      <h3 className="text-6xl text-yellow-700">Sitio en construcción</h3>
     </div>
   );
 }
