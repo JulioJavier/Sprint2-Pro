728bd0c3c32789cc610e88c15d97c223ac525a8c
JulioJavier <01juliomtz@gmail.com>
2023-01-29 16:00:15 -0500

    feat: actualizacion de funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Title from "./components/Title/Title";
+import TodoInput from "./components/TodoInput/TodoInput";
+import TodoList from "./components/TodoList/TodoList";
+
+
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl text-yellow-700">Lo Sentimos</h1>
-      <h3 className="text-6xl text-yellow-700">Sitio en construcción</h3>
+    <div className="bg-gray-900 min-h-screen text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Title/>
+        <TodoInput/>
+        <TodoList>
+          <h2>TodoList</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </TodoList>
+      </div>
+
     </div>
   );
 }
