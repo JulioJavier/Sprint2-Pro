380a1fd4dd49ea5d3d550fa7f4af0145693953db
JulioJavier <01juliomtz@gmail.com>
2023-01-29 22:13:13 -0500

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import { Title } from "./components/Title/Title";
-import { Todo } from "./components/Todo/Todo";
-import { TodoInput } from "./components/TodoInput";
-import { TodoList } from "./components/TodoList";
+import { useState, useEffect } from "react";
+import { Title, TodoInput, TodoList } from "./components";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -28,16 +25,82 @@ function App() {
     },
   ]);
 
-  const addTodo = ()
+  const [activeFilter, setActiveFilter] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState(todos);
+
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false,
+    };
+
+    const todoList = [...todos];
+    todoList.push(newTodo);
+    setTodos(todoList);
+  };
+
+  const handleSetComplete = (id) => {
+    const updatedList = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = todos.filter(todo => todo.id !== id);
+    setTodos(updatedList);
+  };
+
+  const handleClearComplete = () => {
+    const updatedList = todos.filter(todo => !todo.completed);
+    setTodos(updatedList);
+  };
+
+  const showAllTodos = () => {
+    setActiveFilter("all");
+  };
+
+  const showActiveTodos = () => {
+    setActiveFilter("active");
+  };
+
+  const showCompletedTodos = () => {
+    setActiveFilter("completed");
+  };
+
+  useEffect(() => {
+    if (activeFilter === "all") {
+      setFilteredTodos(todos);
+    } else if (activeFilter === "active") {
+      const activeTodos = todos.filter(todo => todo.completed === false);
+      setFilteredTodos(activeTodos);
+    } else if (activeFilter === "completed") {
+      const completedTodos = todos.filter(todo => todo.completed === true);
+      setFilteredTodos(completedTodos);
+    }
+  }, [activeFilter, todos]);
 
   return (
-    <div className="font-inter bg-gray-900 min-h-screen text-gray-100 flex items-center justify-center py-20 px-5">
+    <div className="font-inter bg-gray-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
-        <TodoInput />
-        <TodoList todos={todos}>
-
-        </TodoList>
+        <TodoInput addTodo={addTodo} />
+        <TodoList
+          activeFilter={activeFilter}
+          todos={filteredTodos}
+          showAllTodos={showAllTodos}
+          showActiveTodos={showActiveTodos}
+          showCompletedTodos={showCompletedTodos}
+          handleSetComplete={handleSetComplete}
+          handleDelete={handleDelete}
+          handleClearComplete={handleClearComplete}
+        />
       </div>
     </div>
   );
--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { FiltersContainer, ItemsLeft, FilterButtonContainer, FilterButton } from './TodoFilters.components'
 
-const TodoFilters = () => {
+const TodoFilters = ({
+    total,
+    activeFilter,
+    handleClearComplete,
+    showAllTodos,
+    showActiveTodos,
+    showCompletedTodos,
+}) => {
     return (
         <FiltersContainer>
-            <ItemsLeft />
+            <ItemsLeft total={total}/>
             <FilterButtonContainer>
-                <FilterButton action={ () => {}} active="All" filter='All' />
-                <FilterButton action={ () => {}} active="All" filter='Active' />
-                <FilterButton action={ () => {}} active="All" filter='Completed' />
+                <FilterButton action={ () => showAllTodos()} active={activeFilter} filter='All' />
+                <FilterButton action={ () => showActiveTodos()} active={activeFilter} filter='Active' />
+                <FilterButton action={ () => showCompletedTodos()} active={activeFilter} filter='Completed' />
             </FilterButtonContainer>
-            <button className='text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in'>
+            <button onClick={() => handleClearComplete()} className='text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in-out'>
                 Clear Completed
             </button>
         </FiltersContainer>
