61abd950bcd86fba3db244925d7ea78184d7c3fe
JulioJavier <01juliomtz@gmail.com>
2023-02-08 11:03:03 -0500

    feat: update styles button mode dark

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="">
-      <h1 className="text-white text-2xl font-josefin font-bold tracking-widest text-DarkTheme-LightGrayishBlue">T O D O</h1>
+      <h1 className="text-2xl font-josefin font-bold tracking-widest text-white">T O D O</h1>
     </div>
   );
 };
